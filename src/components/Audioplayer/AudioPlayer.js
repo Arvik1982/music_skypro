af3092ab739d31ec508ea468612098448e4922ee
Arvik1982 <phone.kulikovarseniy@gmail.com>
2023-10-10 10:08:54 +0300

    hw5_11_1

--- src/components/Audioplayer/AudioPlayer.js
+++ src/components/Audioplayer/AudioPlayer.js
@@ -4,112 +4,146 @@ import React, { useEffect, useRef, useState } from "react";
 
 import "react-loading-skeleton/dist/skeleton.css";
 import { SkeletonTheme } from "react-loading-skeleton";
-import '../../img/icon/play.svg'
+import "../../img/icon/play.svg";
 import * as S from "./PlayerStyles";
 
-export function Player({playerVisibility, activeTrack}) {
+export function Player({ playerVisibility, activeTrack }) {
+  const realPlayer = useRef(null);
+  const [playerOn, setPlayerOn] = useState(false);
+  const [loopOn, setLoopOn] = useState(false);
+  const [volumeOn, setVolumeOn] = useState(0.2);
+  const [progressOn, setProgressOn] = useState(0);
+  const [trackTime, setTrackTime] = useState(0);
+  const [timePlay, setTimePlay] = useState(0);
 
-const realPlayer=useRef(null)
-const[playerOn, setPlayerOn]= useState(false)
-const[loopOn, setLoopOn]= useState(false)
-const[volumeOn, setVolumeOn]= useState(0.2)
-const[progressOn, setProgressOn]= useState(0)
-const[trackTime, setTrackTime]= useState(0)
-const[timePlay, setTimePlay]=useState(0)
+  // let trackDuration = activeTrack.duration_in_seconds
 
-// let trackDuration = activeTrack.duration_in_seconds
+  useEffect(() => {
+    setTimeout(() => {
+      realPlayer.current.addEventListener("timeupdate", () => {
+        setProgressOn(realPlayer.current.currentTime);
+      });
+    }, 1000);
+    setTimeout(() => {
+      realPlayer.current.addEventListener("loadedmetadata", () => {
+        setTrackTime(realPlayer.current.duration);
+      });
+    }, 1);
+    return () => {
+      realPlayer.current.removeEventListener("timeupdate", () => {
+        setProgressOn(realPlayer.current.currentTime);
+      });
+      realPlayer.current.removeEventListener("loadedmetadata", () => {
+        setTrackTime(realPlayer.current.duration);
+      });
+    };
+  }, []);
 
-useEffect(()=>{setTimeout(()=>{
-realPlayer.current.addEventListener('timeupdate', () => {
-setProgressOn(realPlayer.current.currentTime);
-})},1000);
-setTimeout(()=>{realPlayer.current.addEventListener('loadedmetadata',()=>{
-setTrackTime(realPlayer.current.duration)})}, 1)
-return ()=>{realPlayer.current.removeEventListener('timeupdate', () => {
-  setProgressOn(realPlayer.current.currentTime)});realPlayer.current.removeEventListener('loadedmetadata', () => {
-    setTrackTime(realPlayer.current.duration)})}
-},[])
+  const clickPlayerStart = () => {
+    realPlayer.current.play();
+    setPlayerOn(true);
+    console.log(realPlayer);
+    console.log("click");
+  };
+  const clickPlayerStop = () => {
+    realPlayer.current.pause();
+    setPlayerOn(false);
+  };
+  const clickPlayerLoopOn = () => {
+    realPlayer.current.loop = true;
+    setLoopOn(true);
+  };
+  const clickPlayerLoopOff = () => {
+    realPlayer.current.loop = false;
+    setLoopOn(false);
+  };
 
-
-
-  const clickPlayerStart=()=>{
-   
-    realPlayer.current.play()
-    setPlayerOn(true)
-    console.log(realPlayer)
-    console.log('click')
-  }
-  const clickPlayerStop=()=>{
-    realPlayer.current.pause()
-    setPlayerOn(false)
-  }
-  const clickPlayerLoopOn=()=>{
-    realPlayer.current.loop=true
-     setLoopOn(true)
-  }  
-  const clickPlayerLoopOff=()=>{
-    realPlayer.current.loop=false
-     setLoopOn(false)
-  } 
-
-  
-  
-  
   const [contentVisible, setContentVisible] = useState(false);
   setTimeout(() => {
     setContentVisible(true);
   }, 500);
 
   return (
-    <S.bar style={{visibility:`${playerVisibility}`}}>
+    <S.bar style={{ visibility: `${playerVisibility}` }}>
       <S.barContent>
-      <audio hidden id="audio" controls ref={realPlayer} src={activeTrack.track_file} style={{marginBottom:'20px'}}>AudioPlayer</audio>
-        <S.progress 
-        
-        type="range"
-        step={0.1}
-        min={0}
-        
-        onChange={(e)=> {realPlayer.current.currentTime = e.target.value; setProgressOn(e.target.value) }}
-        value={progressOn}
-        max={trackTime}
-        
+        <audio
+          hidden
+          id="audio"
+          controls
+          ref={realPlayer}
+          src={activeTrack.track_file}
+          style={{ marginBottom: "20px" }}
+        >
+          AudioPlayer
+        </audio>
+        <S.progress
+          type="range"
+          step={0.1}
+          min={0}
+          onChange={(e) => {
+            realPlayer.current.currentTime = e.target.value;
+            setProgressOn(e.target.value);
+          }}
+          value={progressOn}
+          max={trackTime}
         ></S.progress>
-        
+
         <S.playerBlock>
-        
           <S.barPlayer_player>
-          <div><h2 style={{marginLeft:'20px', color:'gray'}}>{((trackTime-progressOn)/60).toFixed(2)} </h2></div>
+            <div>
+              <h2 style={{ marginLeft: "20px", color: "gray" }}>
+                {((trackTime - progressOn) / 60).toFixed(2)}{" "}
+              </h2>
+            </div>
             <S.playerControls>
-            
               <S.playerBtnPrev>
-                <S.playerBtnPrevSvg onClick={()=>alert('не реализовано')} alt="prev">
+                <S.playerBtnPrevSvg
+                  onClick={() => alert("не реализовано")}
+                  alt="prev"
+                >
                   <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
                   <use href={`${sprite}#icon-prev`} />
                 </S.playerBtnPrevSvg>
               </S.playerBtnPrev>
-              
+
               <S.playerBtnPlay className="_btn">
-                <S.playerBtnPlaySvg onClick={playerOn? clickPlayerStop:clickPlayerStart} alt="play">
+                <S.playerBtnPlaySvg
+                  onClick={playerOn ? clickPlayerStop : clickPlayerStart}
+                  alt="play"
+                >
                   {/* <use xlinkHref="img/icon/sprite.svg#icon-play"></use> */}
-                  <use href= {playerOn?`${sprite}#icon-pause`:`${sprite}#icon-play`}                
+                  <use
+                    href={
+                      playerOn ? `${sprite}#icon-pause` : `${sprite}#icon-play`
+                    }
                   />
                 </S.playerBtnPlaySvg>
               </S.playerBtnPlay>
               <S.playerBtnNext>
-                <S.playerBtnNextSvg onClick={()=>alert('не реализовано')} alt="next">
+                <S.playerBtnNextSvg
+                  onClick={() => alert("не реализовано")}
+                  alt="next"
+                >
                   <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
                   <use href={`${sprite}#icon-next`} />
                 </S.playerBtnNextSvg>
               </S.playerBtnNext>
               <S.playerBtnRepeat className="_btn-icon">
-                <S.playerBtnRepeatSvg onClick={loopOn?clickPlayerLoopOff:clickPlayerLoopOn} alt="repeat">
+                <S.playerBtnRepeatSvg
+                  onClick={loopOn ? clickPlayerLoopOff : clickPlayerLoopOn}
+                  alt="repeat"
+                >
                   {/* <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use> */}
-                  <use  href={loopOn?`${sprite}#loopOn`:`${sprite}#icon-repeat`} />
+                  <use
+                    href={loopOn ? `${sprite}#loopOn` : `${sprite}#icon-repeat`}
+                  />
                 </S.playerBtnRepeatSvg>
               </S.playerBtnRepeat>
-              <S.playerBtnShuffle  className=" _btn-icon">
-                <S.playerBtnShuffleSvg onClick={()=>alert('не реализовано')} alt="shuffle">
+              <S.playerBtnShuffle className=" _btn-icon">
+                <S.playerBtnShuffleSvg
+                  onClick={() => alert("не реализовано")}
+                  alt="shuffle"
+                >
                   {/* <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use> */}
                   <use href={`${sprite}#icon-shuffle`} />
                 </S.playerBtnShuffleSvg>
@@ -180,8 +214,13 @@ return ()=>{realPlayer.current.removeEventListener('timeupdate', () => {
                 </S.playerBarVolSvg>
               </S.playerBarVolImg>
               <S.playerBarVolProgress className="_btn">
-              
-                <S.playerBarVolProgressLine onChange={(e)=>{ let volumeRange=(e.target.value)/100;realPlayer.current.volume=volumeRange; setVolumeOn(volumeRange); console.log(volumeOn)}} 
+                <S.playerBarVolProgressLine
+                  onChange={(e) => {
+                    let volumeRange = e.target.value / 100;
+                    realPlayer.current.volume = volumeRange;
+                    setVolumeOn(volumeRange);
+                    console.log(volumeOn);
+                  }}
                   className="_btn"
                   type="range"
                   name="range"
--- src/components/Content/ContentBlock.js
+++ src/components/Content/ContentBlock.js
@@ -7,31 +7,58 @@ import * as S from "./ContentStyle.js";
 import { getTracks } from "../../api";
 import { Link } from "react-router-dom";
 
-
-let errorText=null;
+let errorText = null;
 let href;
-let tracks =[{id:'1'},{id:'2'},{id:'3'},{id:'4'},{id:'5'},{id:'6'},{id:'7'},{id:'8'},{id:'9'},{id:'101'},{id:'11'},{id:'12'},{id:'13'},{id:'14'},{id:'15'},{id:'16'},{id:'17'},{id:'18'},{id:'19'},{id:'20'},{id:'21'}]
-export function Content({activeTrack, setActiveTrack,  setPlayerOn}) {
-  
+let tracks = [
+  { id: "1" },
+  { id: "2" },
+  { id: "3" },
+  { id: "4" },
+  { id: "5" },
+  { id: "6" },
+  { id: "7" },
+  { id: "8" },
+  { id: "9" },
+  { id: "10" },
+  { id: "11" },
+  { id: "12" },
+  { id: "13" },
+  { id: "14" },
+  { id: "15" },
+  { id: "16" },
+  { id: "17" },
+  { id: "18" },
+  { id: "19" },
+  { id: "20" },
+  { id: "21" },
+];
+export function Content({ activeTrack, setActiveTrack, setPlayerOn }) {
   const [contentVisible, setContentVisible] = useState(false);
 
-  useEffect(()=>{getTracks().then((data)=>{errorText=null;tracks = data;setContentVisible(true); return tracks})
-  .catch((error) =>{errorText=error.message;setContentVisible(true); tracks=[]; return errorText })},[])
-
+  useEffect(() => {
+    getTracks()
+      .then((data) => {
+        errorText = null;
+        tracks = data;
+        setContentVisible(true);
+        return tracks;
+      })
+      .catch((error) => {
+        errorText = error.message;
+        setContentVisible(true);
+        tracks = [];
+        return errorText;
+      });
+  }, []);
 
   // const [contentVisible, setContentVisible] = useState(false);
   // setTimeout(() => {
   // setContentVisible(true);
   // }, 1000);
-  
 
-   return (
+  return (
     <S.CentralBlockContent>
-
-     
-
       <S.CentralBlock_playlistTitle>
-      
         <S.PlaylistTitleCol01>Трек</S.PlaylistTitleCol01>
         <S.PlaylistTitleCol02>ИСПОЛНИТЕЛЬ</S.PlaylistTitleCol02>
         <S.PlaylistTitleCol03>АЛЬБОМ</S.PlaylistTitleCol03>
@@ -42,114 +69,119 @@ export function Content({activeTrack, setActiveTrack,  setPlayerOn}) {
         </S.PlaylistTitleCol04>
       </S.CentralBlock_playlistTitle>
       <S.CentralBlockContentPlaylist>
+        <div style={{ color: "red" }}>
+          <h1>
+            {errorText !== null
+              ? `Ошибка: ${errorText}, попробуйте позже`
+              : null}
+          </h1>
+        </div>
 
-        <div style={{color:'red'}}><h1>{errorText!==null?`Ошибка: ${errorText}, попробуйте позже`:null}</h1></div>
+        {tracks.map((track) => {
+          return (
+            <S.Playlist__item key={track.id}>
+              {/* block start */}
 
-        {tracks.map((track)=>{return(
-          <S.Playlist__item  key = {track.id}>{/* block start */}
-          
-          <S.Playlist__track
-          
-          onClick={ (e)=>{e.preventDefault()
-                     
-            console.log('player load'); 
-            setPlayerOn('');
-            setActiveTrack(track);
-            }}
-          
-          
-          >
-          
-            <S.Track__title 
-            
-            // onClick={()=>{
-            // console.log('player load'); 
-            // setPlayerOn('');
-            // setActiveTrack(track);
-            // }}
-            
-            >
-              
-              <S.Track__titleImage>
-                {contentVisible ? (
-                  <S.Track__titleSvg alt="music">
-                    <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-                    <use href={`${sprite}#icon-note`} />
-                  </S.Track__titleSvg>
-                ) : (
-                  <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                    <S.Skeleton_square />
-                  </SkeletonTheme>
-                )}
-              </S.Track__titleImage >
-              <S.Track_titleText >
-                <S.Track__titleLink  onClick={()=>{console.log('player load ?'); }} className="trackNameLink" href='http://'>
+              <S.Playlist__track
+                onClick={(e) => {
+                  e.preventDefault();
+
+                  console.log("player load");
+                  setPlayerOn("");
+                  setActiveTrack(track);
+                }}
+              >
+                <S.Track__title
+
+                // onClick={()=>{
+                // console.log('player load');
+                // setPlayerOn('');
+                // setActiveTrack(track);
+                // }}
+                >
+                  <S.Track__titleImage>
+                    {contentVisible ? (
+                      <S.Track__titleSvg alt="music">
+                        <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+                        <use href={`${sprite}#icon-note`} />
+                      </S.Track__titleSvg>
+                    ) : (
+                      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+                        <S.Skeleton_square />
+                      </SkeletonTheme>
+                    )}
+                  </S.Track__titleImage>
+                  <S.Track_titleText>
+                    <S.Track__titleLink
+                      onClick={() => {
+                        console.log("player load ?");
+                      }}
+                      className="trackNameLink"
+                      href="http://"
+                    >
+                      {contentVisible ? (
+                        <span>{track.name}</span>
+                      ) : (
+                        <SkeletonTheme
+                          baseColor="#202020"
+                          highlightColor="#444"
+                        >
+                          <S.Skeleton_line />
+                        </SkeletonTheme>
+                      )}
+                      <S.Track__titleSpan></S.Track__titleSpan>
+                    </S.Track__titleLink>
+                  </S.Track_titleText>
+                </S.Track__title>
+                <S.Track__author>
+                  <S.Track__authorLink href="http://">
+                    {contentVisible ? (
+                      <span>{track.author}</span>
+                    ) : (
+                      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+                        <S.Skeleton_line />
+                      </SkeletonTheme>
+                    )}
+                  </S.Track__authorLink>
+                </S.Track__author>
+                <S.Track__album>
+                  <S.Track__albumLink href="http://">
+                    {contentVisible ? (
+                      <span>{track.album}</span>
+                    ) : (
+                      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+                        <S.Skeleton_line />
+                      </SkeletonTheme>
+                    )}
+                  </S.Track__albumLink>
+                </S.Track__album>
+                <S.Track_time>
                   {contentVisible ? (
-                    <span>{track.name}</span>
+                    <S.Track__timeSvg alt="time">
+                      <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+                      <use href={`${sprite}#icon-like`} />
+                    </S.Track__timeSvg>
                   ) : (
                     <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                      <S.Skeleton_line />
+                      <S.Skeleton_lineMini />
                     </SkeletonTheme>
                   )}
-                  <S.Track__titleSpan></S.Track__titleSpan>
-                </S.Track__titleLink>
-              </S.Track_titleText>
-            </S.Track__title>
-            <S.Track__author>
-              <S.Track__authorLink href="http://">
-                {contentVisible ? (
-                  <span>{track.author}</span>
-                ) : (
-                  <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                    <S.Skeleton_line />
-                  </SkeletonTheme>
-                )}
-              </S.Track__authorLink>
-            </S.Track__author>
-            <S.Track__album>
-              <S.Track__albumLink href="http://">
-                {contentVisible ? (
-                  <span>{track.album}</span>
-                ) : (
-                  <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                    <S.Skeleton_line />
-                  </SkeletonTheme>
-                )}
-              </S.Track__albumLink>
-            </S.Track__album>
-            <S.Track_time>
-              {contentVisible ? (
-                <S.Track__timeSvg alt="time">
-                  <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-                  <use href={`${sprite}#icon-like`} />
-                </S.Track__timeSvg>
-              ) : (
-                <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                  <S.Skeleton_lineMini />
-                </SkeletonTheme>
-              )}
 
-              {contentVisible ? (
-                <S.Track__timeText>{track.duration_in_seconds}</S.Track__timeText>
-              ) : (
-                <SkeletonTheme baseColor="#202020" highlightColor="#444">
-                  <S.Skeleton_displayNo />
-                </SkeletonTheme>
-              )}
-            </S.Track_time>
-          </S.Playlist__track>
-        </S.Playlist__item>
-
-
-        )})}
-
-
-
-
-
-        
+                  {contentVisible ? (
+                    <S.Track__timeText>
+                      {track.duration_in_seconds}
+                    </S.Track__timeText>
+                  ) : (
+                    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+                      <S.Skeleton_displayNo />
+                    </SkeletonTheme>
+                  )}
+                </S.Track_time>
+              </S.Playlist__track>
+            </S.Playlist__item>
+          );
+        })}
       </S.CentralBlockContentPlaylist>
-       
     </S.CentralBlockContent>
   );
 }
